2459feb113d77875f91c229db0b37ca97a3904b9
Kaleem Mehdi <kaleemmehdi@Kaleems-Air.home>
2025-04-19 17:19:28 -0400

    final project

--- public/assets/js/homepage.js
+++ public/assets/js/homepage.js
@@ -1,1 +1,7 @@
 console.log('calling homepage')
+
+document.addEventListener('DOMContentLoaded', function() {
+    const hour = new Date().getHours();
+    const status = document.getElementById("office-status");
+    status.textContent = (hour >= 9 && hour < 19) ? "CURRENT STATUS: OPEN UNTIL 7 PM" : "CURRENT STATUS: CLOSED UNTIL 9 AM";
+});
